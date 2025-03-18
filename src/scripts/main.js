ae1761848e536e64973b615afc51cc1a3a79a9f0
Ivan Sierov <iserov1989@gmail.com>
2025-03-19 00:30:06 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,8 +16,14 @@ document.addEventListener('click', (e) => {
   let newLeft = clickX - wallRect.left - spiderRect.width / 2;
   let newTop = clickY - wallRect.top - spiderRect.height / 2;
 
-  newLeft = Math.max(0, Math.min(newLeft, wallRect.width - spiderRect.width));
-  newTop = Math.max(0, Math.min(newTop, wallRect.height - spiderRect.height));
+  // const widthBorder = document.getComputedStyle(wall).borderLeftWidth;
+
+  const minCoord = 0;
+  const maxCoordX = wallRect.width - spiderRect.width - 20;
+  const maxCoordY = wallRect.height - spiderRect.height - 20;
+
+  newLeft = Math.max(minCoord, Math.min(newLeft, maxCoordX));
+  newTop = Math.max(minCoord, Math.min(newTop, maxCoordY));
 
   spider.style.left = `${newLeft}px`;
   spider.style.top = `${newTop}px`;
