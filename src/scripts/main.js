d7ddbffa9b91cc7494bf8130e1105ac9090f1816
Ivan Sierov <iserov1989@gmail.com>
2025-03-19 01:27:43 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,10 @@
 'use strict';
 
-'use strict';
-
 document.addEventListener('click', (e) => {
   const wall = document.querySelector('.wall');
   const spider = document.querySelector('.spider');
 
   const wallRect = wall.getBoundingClientRect();
-
   const spiderRect = spider.getBoundingClientRect();
 
   const clickX = e.clientX;
@@ -16,11 +13,12 @@ document.addEventListener('click', (e) => {
   let newLeft = clickX - wallRect.left - spiderRect.width / 2;
   let newTop = clickY - wallRect.top - spiderRect.height / 2;
 
-  // const widthBorder = document.getComputedStyle(wall).borderLeftWidth;
+  const widthBorder =
+    parseFloat(window.getComputedStyle(wall).borderLeftWidth) * 2;
 
   const minCoord = 0;
-  const maxCoordX = wallRect.width - spiderRect.width - 20;
-  const maxCoordY = wallRect.height - spiderRect.height - 20;
+  const maxCoordX = wallRect.width - spiderRect.width - widthBorder;
+  const maxCoordY = wallRect.height - spiderRect.height - widthBorder;
 
   newLeft = Math.max(minCoord, Math.min(newLeft, maxCoordX));
   newTop = Math.max(minCoord, Math.min(newTop, maxCoordY));
