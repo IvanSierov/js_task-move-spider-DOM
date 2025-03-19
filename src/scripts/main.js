cd3b8d7c6608003680d89f0e5bd226dec7df1b5d
Ivan Sierov <iserov1989@gmail.com>
2025-03-19 23:45:16 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,17 +12,23 @@ document.addEventListener('click', (e) => {
   const clickX = e.clientX;
   const clickY = e.clientY;
 
-  let newLeft = clickX - wallRect.left - spiderRect.width / 2 - borderWidth;
-  let newTop = clickY - wallRect.top - spiderRect.height / 2 - borderWidth;
+  if (
+    clickX >= wallRect.left &&
+    clickX <= wallRect.right &&
+    clickY >= wallRect.top &&
+    clickY <= wallRect.bottom
+  ) {
+    let newLeft = clickX - wallRect.left - spiderRect.width / 2 - borderWidth;
+    let newTop = clickY - wallRect.top - spiderRect.height / 2 - borderWidth;
 
-  const minCoord = 0;
+    const minCoord = 0;
+    const maxCoordX = wallRect.width - spiderRect.width - borderWidth * 2;
+    const maxCoordY = wallRect.height - spiderRect.height - borderWidth * 2;
 
-  const maxCoordX = wallRect.width - spiderRect.width - borderWidth * 2;
-  const maxCoordY = wallRect.height - spiderRect.height - borderWidth * 2;
+    newLeft = Math.max(minCoord, Math.min(newLeft, maxCoordX));
+    newTop = Math.max(minCoord, Math.min(newTop, maxCoordY));
 
-  newLeft = Math.max(minCoord, Math.min(newLeft, maxCoordX));
-  newTop = Math.max(minCoord, Math.min(newTop, maxCoordY));
-
-  spider.style.left = `${newLeft}px`;
-  spider.style.top = `${newTop}px`;
+    spider.style.left = `${newLeft}px`;
+    spider.style.top = `${newTop}px`;
+  }
 });
